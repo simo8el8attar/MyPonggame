7ef7f2d6b218c60ce930111d109d08b8d2712647
simo8el8attar <124779861+simo8el8attar@users.noreply.github.com>
2024-03-13 01:16:14 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,9 +11,13 @@ let go;
 function setup() {
 	new Canvas(1270, 500);
   gameFloor1 =new Sprite(5, 245, 10, 500, 'static');
+  gameFloor1.color = 'magenta';
   gameFloor2 =new Sprite(1265, 245, 10, 500, 'static');
+  gameFloor2.color = 'magenta';
   gameFloor3 =new Sprite(0, 5, 2550, 10, 'static');
+  gameFloor3.color = 'magenta';
   gameFloor4 =new Sprite(0, 495, 2550, 10, 'static');
+  gameFloor4.color = 'magenta';
   s = createP(score); // Create a paragraph element for displaying the score
   s.position(600, 200);
   s.style('color', 'white');
@@ -22,10 +26,13 @@ function setup() {
   ball = new Sprite(800,300,30)
   ball.vel.x = -6;
   ball.vel.y = 0.1;
-}
+  ball.speed = 5;
 
+}
+let Speed = 5;
 function draw() {
 	background('black');
+
   frameRate(60);
 
   box1.y = mouse.y
@@ -33,21 +40,27 @@ function draw() {
   
   if (box1.collides(ball)) {
 		ball.vel.x = 6;
-    ball.vel.y =random(-3,1);
+    ball.vel.y =random([-3,3]);
+    ball.speed = Speed;
     score += 1;
     s.html(score);
+    Speed += 1;
 	}
   if (box2.collides(ball)) {
     ball.vel.x = -6;
-    ball.vel.y =random(-3,1);
+    ball.vel.y =random([-3,3]);
+    ball.speed = Speed;
     score +=1;
     s.html(score);
+    Speed += 1;
 	}
   if (gameFloor3.collides(ball)) {
-    ball.speed = 8;
+    ball.speed = Speed;
+    ball.vel.y =random([-3,3]);
 	}
   if (gameFloor4.collides(ball)) {
-    ball.speed = 8;
+    ball.speed = Speed;
+    ball.vel.y =random([-3,3]);
 	}
   if (gameFloor1.collides(ball) || gameFloor2.collides(ball)) {
     noLoop();
